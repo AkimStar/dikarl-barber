7be079e10af3153076d4e94e2f574ffd6984c673
AkimStar <130581616+AkimStar@users.noreply.github.com>
2025-05-13 10:46:56 +0300

    Fix webpack cache error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+    return config;
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
